0eb3864ebe4c194db16734a655dcf2eff93f46c5
Jialin <Jialin.Ouyang@gmail.com>
2017-10-08 11:50:55 -0700

    HDU 1000

---
+++ 2017.10/2017.10.07 - unsorted/HDU1000.java
@@ -0,0 +1,14 @@
+package main;
+
+import template.io.QuickScanner;
+import template.io.QuickWriter;
+
+public class HDU1000 {
+  public void solve(int testNumber, QuickScanner in, QuickWriter out) {
+    try {
+      while (true) {
+        System.out.println(in.nextInt() + in.nextInt());
+      }
+    } catch (RuntimeException e) { }
+  }
+}
